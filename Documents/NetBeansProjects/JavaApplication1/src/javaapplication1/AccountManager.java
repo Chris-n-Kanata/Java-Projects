c20c297fa9a656ef66dd0f1ee67b28d4917dc34e
Chris-n-Kanata <chris.corman@targetfuel.com>
2017-07-18 23:43:44 -0400

    More Periods

--- Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/AccountManager.java
+++ Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/AccountManager.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * 
- * Adding the Account Manager
+ * Adding the Account Manager...
  */
 
 package javaapplication1;
